0eb7f897e5487d0bd569ca070381511789d99233
Leyu <zhang.ley@husky.neu.edu>
2018-07-23 13:57:23 -0400

    added test file for controller

---
+++ test/IEasyAnimatorControllerTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+/**
+ * JUnit test for IEasyAnimatorController interface. It tests the start method of the controller
+ * that adds information to the easy animator model, and return the view written in a file as
+ * required in the command line.
+ */
+public class IEasyAnimatorControllerTest {
+
+
+}
